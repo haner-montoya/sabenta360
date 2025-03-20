4ff3e33956b41db55e3ef7c0f24b1b916f53108c
Haner Montoya <haner.montoya@gmail.com>
2025-03-19 21:13:30 -0500

    First Commit

---
+++ core/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+from django.views.generic import TemplateView
+# Create your views here.
+
+class HomePageView(TemplateView):
+    template_name = 'core/index.html'
+
+    dict_context = {
+        'title': '',
+        'message': ''
+    }
+
+    def get(self, request, *args, **kwargs):
+        """Método que se encarga de devolver la vista"""
+        return render(request, self.template_name, self.dict_context)
