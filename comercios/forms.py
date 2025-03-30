17e9d9e4c115830a21d18c277c371d07a60e3cbe
Eder Lara <Ederlatru@gmail.com>
2025-03-30 11:41:38 -0500

    Mejoras por Eder

--- comercios/forms.py
+++ comercios/forms.py
@@ -2,4 +2,15 @@ from django import forms
 
 
 class RegistroComercio(forms.ModelForm):
-    pass
+    
+    class Meta:
+        model = Comercio
+        fields = '__all__'
+        widgets = {
+            'nombre_comercio': forms.TextInput(attrs={'class': 'form-control'}),
+            'categoria': forms.Select(attrs={'class': 'form-control'}),
+            'direccion': forms.TextInput(attrs={'class': 'form-control'}),
+            'telefono': forms.TextInput(attrs={'class': 'form-control'}),
+            'email': forms.EmailInput(attrs={'class': 'form-control'}),
+            'imagen': forms.ClearableFileInput(attrs={'class': 'form-control'}),
+        }
