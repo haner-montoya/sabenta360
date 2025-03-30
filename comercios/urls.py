f27f8b51cc8dab82bf6916dc1a343c84f18f0cc2
Eder Lara <Ederlatru@gmail.com>
2025-03-30 16:39:48 -0500

    Mejoras de app

--- comercios/urls.py
+++ comercios/urls.py
@@ -3,6 +3,7 @@ from .views import ComercioListview, RestaurantesListview, ModaListview
 
 urlpatterns = [
     path('', ComercioListview.as_view(), name='comercios'),
+    path('nuevo/', ComercioCreateView.as_view(), name='registro_comercio'),
     path('restaurantes/', RestaurantesListview.as_view(), name= 'restaurantes'),
     path('Moda/', ModaListview.as_view(), name= 'Moda'),
 ]
--- comercios/views.py
+++ comercios/views.py
@@ -1,8 +1,27 @@
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.views.generic import ListView, CreateView
+from django.views.generic.edit import CreateView
+from django.urls import reverse_lazy
 from .models import Comercio
+from .forms import RegistroComercio
 
 # Create your views here.
+
+class ComercioCreateView(CreateView):
+    template_name = 'comercios/registro.html'
+    model = Comercio
+    form_class = RegistroComercio
+    # success_url = '/comercios/catalogo/'
+    success_url = reverse_lazy('comercios')
+    
+    def form_valid(self, form):
+        # Aquí puedes agregar lógica adicional antes de guardar el formulario
+        # Por ejemplo, puedes enviar un correo electrónico o realizar alguna validación
+        return super().form_valid(form)
+    
+    def get(self, request, *args, **kwargs):
+        form = self.get_form()
+        return render(request, self.template_name, {'form': self.form_class})
 
 class ComercioListview(ListView):
     template_name = 'comercios/catalogo.html'
