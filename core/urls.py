ee07d626a95981ab766445d6961189256d284d5c
Haner Montoya <haner.montoya@gmail.com>
2025-03-31 21:24:45 -0500

    Cambios ultimo dia

--- core/urls.py
+++ core/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
-from .views import HomePageView, NosotrosPageView, AcercaPageView
+from .views import HomePageView, NosotrosPageView, AcercaPageView, InicioPageView
 
 urlpatterns = [
     path('', HomePageView.as_view(), name='inicio'),
     path('nosotros/', NosotrosPageView.as_view(), name='nosotros'),
-    path('acerca/', AcercaPageView.as_view(), name='acerca')
+    path('acerca/', AcercaPageView.as_view(), name='acerca'),
+    path('inicio/', InicioPageView.as_view(), name='inicio')
+
 ]
 
--- core/views.py
+++ core/views.py
@@ -25,4 +25,10 @@ class AcercaPageView(TemplateView):
 
     def get(self, request, *args, **kwargs):
         return render(request, self.template_name)
+    
+class InicioPageView(TemplateView):
+    template_name = 'core/index.html'
+
+    def get(self, request, *args, **kwargs):
+        return render(request, self.template_name)
 
